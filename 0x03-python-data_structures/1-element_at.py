ba7c9d42796990caf98bd3fb4499d8fe26d44bde
Ajibose <ajiboseibrahim12@gmail.com>
2023-03-14 10:12:03 -0700

    Task1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -6,7 +6,7 @@ def element_at(my_list, idx):
     """
 
     length = len(my_list)
-    if idx < 0 || idx > length - 1:
+    if idx < 0 or idx > length - 1:
         return None
 
     return my_list[idx]

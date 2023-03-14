1b726ee1cf2ba7dcbb9143a8e83ff54c065d1820
Ajibose <ajiboseibrahim12@gmail.com>
2023-03-14 10:16:38 -0700

    Task2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    """
+        Replaces an elent in the idx position in my_list list
+    """
+
+    length = len(my_list)
+    if idx < 0 or idx > length - 1:
+        return my_list
+
+    mylist[idx] = element

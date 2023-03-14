566c97276a97dcf2f34b63c4fa187397dcf6f82b
Ajibose <ajiboseibrahim12@gmail.com>
2023-03-14 05:09:35 -0700

    Task4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import hidden_4
     lists = dir(hidden_4)
-    for item in lists:
+    for item in sorted(lists):
         if item.startswith('_'):
             continue
         print({:s}.format(item))

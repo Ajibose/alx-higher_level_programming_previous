fe3c501bb78cf333f75a7c39af2428d8d66acba6
Ajibose <ajiboseibrahim12@gmail.com>
2023-03-16 09:00:35 -0700

    Task9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -6,10 +6,8 @@ def max_integer(my_list=[]):
 
     if len(my_list) == 0:
         return None
-    elif len(my_list) == 1:
-        max_val = my_list[0]
 
-    max_val = 0
+    max_val = my_list[0]
     for item in my_list:
         if item > max_val:
             max_val = item

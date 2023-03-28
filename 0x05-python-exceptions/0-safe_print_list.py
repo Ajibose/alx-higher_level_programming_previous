a90e8d48f0b8d49ddb6af2d034c2164b893f1496
Ajibose <ajiboseibrahim12@gmail.com>
2023-03-28 10:23:36 -0700

    prints x elements of a list.

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    index = 0
+
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            index += 1
+        except Exception:
+            break
+
+    print()
+    return index

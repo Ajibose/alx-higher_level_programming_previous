4b4e16657a72ed532b33284690b93cbc422f3e3a
Ajibose <ajiboseibrahim12@gmail.com>
2023-03-31 04:28:10 -0700

    Set the setter and getter method

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+    A module that defines a class square
+"""
+
+
+class Square():
+    """
+    A class that defines a square
+    """
+    def __init__(self, size=0):
+        """
+            Initializes a square
+
+            Args:
+                size(int): The length of the square
+        """
+        self.__size = size
+
+    def area(self):
+        """Computes the area of a square
+
+            Returns:
+                int: Area of square
+        """
+        return self.__size ** 2
+
+    @size.setter
+    def size(self, value):
+        """
+            Gets the square size
+        """
+        if type(size) == int:
+            if (size > 0):
+                self.__size = size
+            else:
+                raise ValueError("Size must be >= :0")
+        else:
+            raise TypeError("Size must be an int")
+
+    @property
+    def size(self):
+        """Retrieve te size of the square"""
+        return self.__size

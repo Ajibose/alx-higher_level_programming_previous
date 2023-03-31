b7b2b3ae9eb4ec247236969e9f5714073df02df8
Ajibose <ajiboseibrahim12@gmail.com>
2023-03-31 03:52:46 -0700

    Checks for exception in the square class

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    A module that defines a class square
+"""
+
+
+class Square():
+    """
+    A class that defines a square
+    """
+    def __init__(self, size=0):
+        """
+            Initializes a square
+        """
+        if type(size) == int:
+            if (size > 0):
+                self.__size = size
+            else:
+                raise ValueError("Size must be >= :0")
+        else:
+            raise TypeError("Size must be an int")

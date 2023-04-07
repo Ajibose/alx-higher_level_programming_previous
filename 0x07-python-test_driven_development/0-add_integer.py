1503f52e524c5ed818b7e71c5a368baaaec9c350
Ajibose <ajiboseibrahim12@gmail.com>
2023-04-07 07:23:03 -0700

    Task0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,7 +12,7 @@ def add_integer(a, b=98):
     s"""
     if not type(a) == int and not type(a) == float:
         raise TypeError("a must be an integer")
-    if not type(b)== int and not type(b) == float:
+    if not type(b) == int and not type(b) == float:
         raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)

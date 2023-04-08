3a879f1321103a805f8864e3e6aa81d167e75b5e
Ajibose <ajiboseibrahim12@gmail.com>
2023-04-08 05:55:15 -0700

    a function that divides all elements of a matrix.

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+
+    Defines a function that divides all elements of a matrix
+
+"""
+
+
+def matrix_divided(matrix, div):
+    """divides all elements of a matrix"""
+    result = True
+    size = len(matrix[0])
+    length = True
+    new_matrix = []
+
+    for i in matrix:
+        if len(i) != size:
+            length = False
+        for j in i:
+            if not isinstance(j, int) and not isinstance(j, float):
+                result = False
+
+    if result == False:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    
+    if length == False:
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    
+    new_matrix = list(map(lambda x: list(map(lambda y: round(y / div, 2), x)), matrix))
+    return new_matrix

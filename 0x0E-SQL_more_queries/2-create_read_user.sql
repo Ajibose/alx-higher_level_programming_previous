bdabdd2abceec488810d7a3b65f1341e6c2ab6b7
Ajibose <ajiboseibrahim12@gmail.com>
2023-05-18 15:44:34 -0700

    a script that creates a database and a uesr with SELECT priilege

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- a script that creates the database hbtn_0d_2 and the user user_0d_2.
 CREATE IF NOT EXISTS DATABASE 'hbtn_0d_2';
-CREATE IF NOT EXISTS USER 'user_0d_2';
+CREATE IF NOT EXISTS USER 'user_0d_2' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON 'hbtn_0d_2' TO 'user_0d_2'@'localhost';
